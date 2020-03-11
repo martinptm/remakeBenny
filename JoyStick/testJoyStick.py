bab7ac9bb7056f5482660b3da7521b764cc1931d
Martin <martinkrueck@gmx.de>
2020-03-11 11:54:01 +0100

    read serial description mac

---
+++ JoyStick/testJoyStick.py
@@ -0,0 +1,5 @@
+"""
+Read serial-Input in mac-Termial with $screen /dev/tty.[yourSerialPortName] [yourBaudRate]
+Find connected serial ports (mac and Linux) with $ ls /dev/tty.*
+(https://stackoverflow.com/questions/12254378/how-to-find-the-serial-port-number-on-mac-os-x)
+"""
