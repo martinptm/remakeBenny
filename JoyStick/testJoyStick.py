2aa4278fc979d08a58cc9623737dd376bec2789e
Martin <martinkrueck@gmx.de>
2020-03-16 15:16:51 +0100

    removed unneccesaary files and folders

--- JoyStick/testJoyStick.py
+++
@@ -1,5 +0,0 @@
-"""
-Read serial-Input in mac-Termial with $screen /dev/tty.[yourSerialPortName] [yourBaudRate]
-Find connected serial ports (mac and Linux) with $ ls /dev/tty.*
-(https://stackoverflow.com/questions/12254378/how-to-find-the-serial-port-number-on-mac-os-x)
-"""
